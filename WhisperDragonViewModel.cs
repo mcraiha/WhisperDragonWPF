2282c20954f7cfc407a6480ff8857caa45255710
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-08-24 12:12:24 +0300

    Bind AddLoginWindow to button
    And turn spaces to tabs

--- WhisperDragonViewModel.cs
+++ WhisperDragonViewModel.cs
@@ -6,75 +6,90 @@ using WhisperDragonWPF;
 
 public class WhisperDragonViewModel
 {
-    private TabControl tabSections;
+	private TabControl tabSections;
 
-    public WhisperDragonViewModel(TabControl sections)
-    {
-        this.tabSections = sections;
-    }
+	public WhisperDragonViewModel(TabControl sections)
+	{
+		this.tabSections = sections;
+	}
 
 
-    #region Select tabs
+	#region Select tabs
 
-    private ICommand selectFirstTabCommand;
-    public ICommand SelectFirstTab
-    {
-        get
-        {
-            return selectFirstTabCommand 
-                ?? (selectFirstTabCommand = new ActionCommand(() =>
-                {
-                    //MessageBox.Show("SomeCommand");
-                    this.tabSections.SelectedIndex = 0;
-                }));
-        }
-    }
+	private ICommand selectFirstTabCommand;
+	public ICommand SelectFirstTab
+	{
+		get
+		{
+			return selectFirstTabCommand 
+				?? (selectFirstTabCommand = new ActionCommand(() =>
+				{
+					//MessageBox.Show("SomeCommand");
+					this.tabSections.SelectedIndex = 0;
+				}));
+		}
+	}
 
-    private ICommand selectSecondTabCommand;
-    public ICommand SelectSecondTab
-    {
-        get
-        {
-            return selectSecondTabCommand 
-                ?? (selectSecondTabCommand = new ActionCommand(() =>
-                {
-                    //MessageBox.Show("SomeCommand");
-                    this.tabSections.SelectedIndex = 1;
-                }));
-        }
-    }
+	private ICommand selectSecondTabCommand;
+	public ICommand SelectSecondTab
+	{
+		get
+		{
+			return selectSecondTabCommand 
+				?? (selectSecondTabCommand = new ActionCommand(() =>
+				{
+					//MessageBox.Show("SomeCommand");
+					this.tabSections.SelectedIndex = 1;
+				}));
+		}
+	}
 
-    private ICommand selectThirdTabCommand;
-    public ICommand SelectThirdTab
-    {
-        get
-        {
-            return selectThirdTabCommand 
-                ?? (selectThirdTabCommand = new ActionCommand(() =>
-                {
-                    //MessageBox.Show("SomeCommand");
-                    this.tabSections.SelectedIndex = 2;
-                }));
-        }
-    }
+	private ICommand selectThirdTabCommand;
+	public ICommand SelectThirdTab
+	{
+		get
+		{
+			return selectThirdTabCommand 
+				?? (selectThirdTabCommand = new ActionCommand(() =>
+				{
+					//MessageBox.Show("SomeCommand");
+					this.tabSections.SelectedIndex = 2;
+				}));
+		}
+	}
 
-    #endregion // Select tabs
+	#endregion // Select tabs
 
-    #region Tools
 
-    private ICommand generatePasswordViaMenu;
-    public ICommand GeneratePasswordViaMenu
-    {
-        get
-        {
-            return generatePasswordViaMenu 
-                ?? (generatePasswordViaMenu = new ActionCommand(() =>
-                {
-                    CreatePasswordWindow passwordWindow = new CreatePasswordWindow();
-                    passwordWindow.ShowDialog();
-                }));
-        }
-    }
+	#region Tools
 
-    #endregion // Tools
+	private ICommand generatePasswordViaMenu;
+	public ICommand GeneratePasswordViaMenu
+	{
+		get
+		{
+			return generatePasswordViaMenu 
+				?? (generatePasswordViaMenu = new ActionCommand(() =>
+				{
+					CreatePasswordWindow passwordWindow = new CreatePasswordWindow();
+					passwordWindow.ShowDialog();
+				}));
+		}
+	}
+
+	private ICommand addLoginViaButton;
+	public ICommand AddLoginViaButton
+	{
+		get
+		{
+			return addLoginViaButton 
+				?? (addLoginViaButton = new ActionCommand(() =>
+				{
+					AddLoginWindow addLoginWindow = new AddLoginWindow();
+					addLoginWindow.ShowDialog();
+				}));
+		}
+	}
+
+	#endregion // Tools
 }
