12690fa94a76e357f746c801b0d670c18a494452
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-08-24 12:11:46 +0300

    Add AddLoginWindow
    Includes ViewModel

---
+++ AddLoginViewModel.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.ComponentModel;
+using WhisperDragonWPF;
+
+public class AddLoginViewModel : INotifyPropertyChanged
+{
+	public bool IsSecret { get; set; } = true;
+
+	public string Title { get; set; } = "";
+
+	public string URL { get; set; } = "";
+
+	public string Username { get; set; } = "";
+
+	private bool visiblePassword = false;
+	public bool VisiblePassword 
+	{ 
+		get
+		{
+			return this.visiblePassword;
+		}
+		
+		set
+		{
+			this.visiblePassword = value;
+			// Update to cause onpropertychange
+			Password = password;
+		} 
+	}
+
+	private string password = "";
+	public string Password 
+	{ 
+		get
+		{
+			if (VisiblePassword)
+			{
+				return this.password;
+			}
+			
+			return string.Create(this.password.Length, '*', (chars, buf) => {
+																		for (int i = 0; i < chars.Length; i++) chars[i] = buf;
+					});
+		}
+		set         
+		{
+			this.password = value;
+			OnPropertyChanged(nameof(Password));
+		}
+	
+	}
+
+	public string Notes { get; set; } = "";
+
+	public string Category { get; set; } = "";
+
+	public string Tags { get; set; } = "";
+
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	
+	#region Buttons
+
+	
+	private ICommand addLoginCommand;
+	public ICommand AddLoginCommand
+	{
+		get
+		{
+			return addLoginCommand 
+				?? (addLoginCommand = new ActionCommand(() =>
+				{
+					
+				}));
+		}
+	}
+
+	private ICommand cancelCommand;
+	public ICommand CancelCommand
+	{
+		get
+		{
+			return cancelCommand 
+				?? (cancelCommand = new ActionCommand(() =>
+				{
+					
+				}));
+		}
+	}
+
+	#endregion // Buttons
+
+	#region Property changed
+
+	// Create the OnPropertyChanged method to raise the event
+	protected void OnPropertyChanged(string name)
+	{
+		PropertyChangedEventHandler handler = PropertyChanged;
+		if (handler != null)
+		{
+			handler(this, new PropertyChangedEventArgs(name));
+		}
+	}
+
+	#endregion // Property changed
+}
---
+++ AddLoginWindow.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WhisperDragonWPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class AddLoginWindow : Window
+	{
+		public AddLoginWindow()
+		{
+			InitializeComponent();
+			DataContext = new AddLoginViewModel();
+		}
+
+
+		#region Validators
+
+
+		#endregion // Validators
+
+	}
+}
