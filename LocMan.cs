3f4f09f100c1de6f854907c4efda687fe4e7fe60
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-10-05 18:21:47 +0300

    Add placeholder localization manager class
    It will be needed later on

---
+++ LocMan.cs
@@ -0,0 +1,17 @@
+
+/// <summary>
+/// Localization manager class for handling dynamic localizations
+/// </summary>
+public static class LocMan
+{
+	/// <summary>
+	/// Get localization of one entry
+	/// </summary>
+	/// <param name="id">Id to find</param>
+	/// <returns>Wanted string if it is found; Otherwise error string is returned</returns>
+	public static string Get(string id)
+	{
+		// TODO: Add code once first localization version is out
+		return id;
+	}
+}
