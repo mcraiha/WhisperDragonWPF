f1ae0d276a55a8302d9982b620e0f027882ee688
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-11-17 12:18:50 +0200

    Add TurnIntoUICompatible method
    Needs more configuration, but that will come at later date

--- LoginSimplified.cs
+++ LoginSimplified.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using CSCommonSecrets;
 
 namespace WhisperDragonWPF
 {
@@ -18,5 +20,59 @@ namespace WhisperDragonWPF
 		public string Tags { get; set; }
 		public string CreationTime { get; set; }
 		public string ModificationTime { get; set; }
+
+		public static List<LoginSimplified> TurnIntoUICompatible(List<LoginInformation> loginInformations, List<LoginInformationSecret> loginInformationSecrets, Dictionary<string, byte[]> derivedPasswords)
+		{
+			List<LoginSimplified> returnList = new List<LoginSimplified>();
+
+			if (loginInformations != null && loginInformations.Count > 0)
+			{
+				int runningIndexNumber = 0;
+				foreach (LoginInformation loginInformation in loginInformations)
+				{
+					returnList.Add(new LoginSimplified() {
+						indexNumber = runningIndexNumber,
+						IsSecure = false,
+						Title = loginInformation.GetTitle(),
+						URL = loginInformation.GetURL(),
+						Email = loginInformation.GetEmail(),
+						Username = loginInformation.GetUsername(),
+						Password = loginInformation.GetPassword(),
+						Category = loginInformation.GetCategory(),
+						Tags = loginInformation.GetTags(),
+						CreationTime = loginInformation.GetCreationTime().ToString("s", System.Globalization.CultureInfo.InvariantCulture),
+						ModificationTime = loginInformation.GetModificationTime().ToString("s", System.Globalization.CultureInfo.InvariantCulture),
+					});
+
+					runningIndexNumber++;
+				}
+			}
+
+			if (loginInformationSecrets != null && loginInformationSecrets.Count > 0)
+			{
+				int runningIndexNumber = 0;
+				foreach (LoginInformationSecret loginInformationSecret in loginInformationSecrets)
+				{
+					byte[] derivedPassword = derivedPasswords[loginInformationSecret.GetKeyIdentifier()];
+					returnList.Add(new LoginSimplified() {
+						indexNumber = runningIndexNumber,
+						IsSecure = false,
+						Title = loginInformationSecret.GetTitle(derivedPassword),
+						URL = loginInformationSecret.GetUrl(derivedPassword),
+						Email = loginInformationSecret.GetEmail(derivedPassword),
+						Username = loginInformationSecret.GetUsername(derivedPassword),
+						Password = loginInformationSecret.GetPassword(derivedPassword),
+						Category = loginInformationSecret.GetCategory(derivedPassword),
+						Tags = loginInformationSecret.GetTags(derivedPassword),
+						CreationTime = loginInformationSecret.GetCreationTime(derivedPassword).ToString("s", System.Globalization.CultureInfo.InvariantCulture),
+						ModificationTime = loginInformationSecret.GetModificationTime(derivedPassword).ToString("s", System.Globalization.CultureInfo.InvariantCulture),
+					});
+
+					runningIndexNumber++;
+				}
+			}
+
+			return returnList;
+		}
 	}
 }
