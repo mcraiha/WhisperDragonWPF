228dbe5bfd40da7bdf1d41db5b62d4a12e29a684
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-02-15 17:30:03 +0200

    Add show modes for note titles
    There will be refactoring later

--- src/SettingsData.cs
+++ src/SettingsData.cs
@@ -16,6 +16,7 @@ namespace WhisperDragonWPF
     /// </summary>
     public class SettingsData
     {
+        // Logins
         public ShowMode TitleShowMode { get; set; } = ShowMode.ShowFull;
 
         public ShowMode UrlShowMode { get; set; } = ShowMode.ShowFull;
@@ -27,6 +28,11 @@ namespace WhisperDragonWPF
         public ShowMode PasswordShowMode { get; set; } = ShowMode.HiddenConstantLenght;
 
         public ShowMode CategoryShowMode { get; set; } = ShowMode.ShowFull;
+
+
+        // Notes
+        public ShowMode NoteTitleShowMode { get; set; } = ShowMode.ShowFull;
+        public ShowMode NoteTextShowMode { get; set; } = ShowMode.HiddenConstantLenght;
         
         public SettingsData()
         {
